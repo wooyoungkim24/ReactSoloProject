b8048686bfb53c05251b7409c9a45766e3972548
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-02-04 13:39:14 -0500

    login working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 // router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
 //   const user = await User.findOne({
