437f73df7e9a26d3a60fa84813b9f4d588fc82fb
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-02-04 18:39:23 -0500

    deploying first steps

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -45,8 +45,41 @@ const sessionReducer = (state = initialState, action) => {
       newState.user = null;
       return newState;
     default:
-      return state;
-  }
+            return state;
+    }
+};
+
+
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
 };
 
 export default sessionReducer;
